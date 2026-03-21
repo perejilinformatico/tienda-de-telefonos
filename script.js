5246d9a946e4f2ad31ac93ae9346ee7af19d9695
Santiago <perejilinformatico@gmail.com>
2026-03-20 21:02:32 -0300

    todo

---
+++ script.js
@@ -0,0 +1,20 @@
+let i = document.getElementById("btn-cel");
+let i2 = document.getElementById("btn-cel2");
+let i3 = document.getElementById("btn-cel3");
+let i4 = document.getElementById("btn-cel4");
+
+i.addEventListener("click", () => {
+  alert(i.value);
+});
+
+i2.addEventListener("click", () => {
+  alert(i2.value);
+});
+
+i3.addEventListener("click", () => {
+  alert(i3.value);
+});
+
+i4.addEventListener("click", () => {
+  alert(i4.value);
+});
